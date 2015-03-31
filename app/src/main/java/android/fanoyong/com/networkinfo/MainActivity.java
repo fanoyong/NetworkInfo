3db65f36004ad1aedfa39d81bb9af638fdc8fb9d
Hwanho Yong <hwanho.yong@samsung.com>
2015-03-31 14:42:01 -0400

    Also checking TYPE_MOBILE_HIPRI / Better logging

--- app/src/main/java/android/fanoyong/com/networkinfo/MainActivity.java
+++ app/src/main/java/android/fanoyong/com/networkinfo/MainActivity.java
@@ -1,27 +1,22 @@
 package android.fanoyong.com.networkinfo;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.AsyncTaskLoader;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.net.NetworkRequest;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.UnknownHostException;
 
 
@@ -36,15 +31,17 @@ public class MainActivity extends Activity {
             Log.d(TAG, "ConnectivityInfo :");
             String dump = getConnectivityInfo();
             Log.d(TAG, dump);
+            setTextView();
             Log.d(TAG, "************************************************************");
+
         }
     };
-
 
     private Context mContext;
     private ConnectivityManager mCm;
     private NetworkRequest mNetworkRequest;
     private ConnectivityManager.NetworkCallback mNetworkCallback;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +52,10 @@ public class MainActivity extends Activity {
         Button button1 = (Button) findViewById(R.id.btn_1);
         Button button2 = (Button) findViewById(R.id.btn_2);
         Button button3 = (Button) findViewById(R.id.btn_3);
+        Button button4 = (Button) findViewById(R.id.btn_4);
+        Button button5 = (Button) findViewById(R.id.btn_5);
+        Button button6 = (Button) findViewById(R.id.btn_6);
+        tv1 = (TextView) findViewById(R.id.textview_1);
 
         IntentFilter intentfilter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
         registerReceiver(mConnChangeReceiver, intentfilter);
@@ -69,9 +70,17 @@ public class MainActivity extends Activity {
                         stopMobileMms();
                         break;
                     case R.id.btn_3:
-                        startMobileMmsNew();
+                        requestRoute(ConnectivityManager.TYPE_MOBILE_MMS);
                         break;
-                    default:
+                    case R.id.btn_4:
+                        startHipri();
+                        break;
+                    case R.id.btn_5:
+                        stopHipri();
+                        break;
+                    case R.id.btn_6:
+                        requestRoute(ConnectivityManager.TYPE_MOBILE_HIPRI);
+                        break;
                 }
             }
         };
@@ -79,6 +88,9 @@ public class MainActivity extends Activity {
         button1.setOnClickListener(ocl);
         button2.setOnClickListener(ocl);
         button3.setOnClickListener(ocl);
+        button4.setOnClickListener(ocl);
+        button5.setOnClickListener(ocl);
+        button6.setOnClickListener(ocl);
     }
 
     @Override
@@ -98,9 +110,38 @@ public class MainActivity extends Activity {
         Log.d(TAG, "stopUsingNetworkFeature result=" + result);
     }
 
-    private void startMobileMmsNew() {
-        boolean result = mCm.requestRouteToHost(ConnectivityManager.TYPE_MOBILE_MMS, lookupHost("http://mms.vtext.com/servlets/mms"));
+    private void startHipri() {
+        int result = mCm.startUsingNetworkFeature(ConnectivityManager.TYPE_MOBILE, "enableHIPRI");
         Log.d(TAG, "startUsingNetworkFeature result=" + result);
+    }
+
+    private void stopHipri() {
+        int result = mCm.stopUsingNetworkFeature(ConnectivityManager.TYPE_MOBILE, "enableHIPRI");
+        Log.d(TAG, "stopUsingNetworkFeature result=" + result);
+    }
+
+    private void requestRoute(int type) {
+        new HttpTask().execute(type);
+    }
+
+    private class HttpTask extends AsyncTask<Integer, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Integer[] params) {
+            int type = params[0];
+            boolean result = false;
+            Log.d(TAG, "requestRouteToHost type=" + type);
+            switch (type) {
+                case ConnectivityManager.TYPE_MOBILE_MMS:
+                    result = mCm.requestRouteToHost(ConnectivityManager.TYPE_MOBILE_MMS, lookupHost("http://mms.vtext.com/servlets/mms"));
+                    break;
+                case ConnectivityManager.TYPE_MOBILE_HIPRI:
+                    result = mCm.requestRouteToHost(ConnectivityManager.TYPE_MOBILE_HIPRI, lookupHost("http://mms.vtext.com/servlets/mms"));
+                    break;
+            }
+            Log.d(TAG, "requestRouteToHost result=" + result);
+            return null;
+        }
     }
 
     private int lookupHost(String hostname) {
@@ -124,17 +165,23 @@ public class MainActivity extends Activity {
         StringBuilder sb = new StringBuilder();
         sb.append("getAllNetworkInfo\n");
         sb.append(mCm.getAllNetworkInfo());
-        sb.append("************************************************************\n");
         sb.append("getActiveNetworkInfo\n");
         sb.append(mCm.getActiveNetworkInfo());
-        sb.append("************************************************************\n");
         NetworkInfo[] nis = mCm.getAllNetworkInfo();
         if (nis != null) {
             for (NetworkInfo ni : nis) {
-                sb.append("ni.getType" + ni.getType() + "ni.getState" + ni.getState() + "ni.detailState" + ni.getDetailedState() + "ni.isAvailable" + ni.isAvailable() + "ni.isconnected" + ni.isConnected());
+                sb.append("ni.getType\t" + ni.getType() + "\tni.subType\t" + ni.getSubtypeName() + "\tni.extraInfo\t" + ni.getExtraInfo() + "\tni.getState\t" + ni.getState() + "\tni.detailState\t" + ni.getDetailedState() + "\tni.isAvailable\t" + ni.isAvailable() + "\tni.isConnected\t" + ni.isConnected() + "\n");
             }
         }
-        sb.append("************************************************************\n");
         return sb.toString();
+    }
+
+    private void setTextView() {
+        StringBuilder sb = new StringBuilder();
+        NetworkInfo ni = mCm.getActiveNetworkInfo();
+        if (ni != null) {
+            sb.append("ni.getType " + ni.getType() + " ni.subType " + ni.getSubtypeName() + " ni.extraInfo " + ni.getExtraInfo() + " ni.getState " + ni.getState() + " ni.detailState " + ni.getDetailedState() + " ni.isAvailable " + ni.isAvailable() + " ni.isconnected " + ni.isConnected());
+        }
+        tv1.setText(sb.toString());
     }
 }
